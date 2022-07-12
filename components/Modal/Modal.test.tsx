d17d7d2f0a1eef359908473a869b705931f232b2
Ammar Oker <oker.ammar@gmail.com>
2022-07-12 18:27:43 +0300

    add circleci

---
+++ components/Modal/Modal.test.tsx
@@ -0,0 +1,9 @@
+import { screen } from "@testing-library/react";
+
+describe("<Modal />", ()=> {
+  it("should ...", () => {
+
+    // TODO write unit test
+    expect(true).toBeTruthy()
+  });
+});
